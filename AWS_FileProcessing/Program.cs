57e31ca09f9fdd0f661f26d1281f84865e58e9de
Anna9701 <202340@edu.p.lodz.pl>
2019-04-15 13:11:52 +0200

    Sample file transfer to bucket s3

--- AWS_FileProcessing/Program.cs
+++ AWS_FileProcessing/Program.cs
@@ -24,8 +24,10 @@ namespace AWS_FileProcessing
             }
         }
 
-        private static async Task UploadFileAsync() {
-            try {
+        private static async Task UploadFileAsync()
+        {
+            try
+            {
                 var fileTransferUtility = new TransferUtility(s3Client);
 
                 // Option 1. Upload a file. The file name is used as the object key name.
@@ -37,13 +39,15 @@ namespace AWS_FileProcessing
                 Console.WriteLine("Upload 2 completed");
 
                 // Option 3. Upload data from a type of System.IO.Stream.
-                using (var fileToUpload = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
+                using (var fileToUpload = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
                     await fileTransferUtility.UploadAsync(fileToUpload, bucketName, keyName);
                 }
                 Console.WriteLine("Upload 3 completed");
 
                 // Option 4. Specify advanced settings.
-                var fileTransferUtilityRequest = new TransferUtilityUploadRequest {
+                var fileTransferUtilityRequest = new TransferUtilityUploadRequest
+                {
                     BucketName = bucketName,
                     FilePath = filePath,
                     StorageClass = S3StorageClass.StandardInfrequentAccess,
@@ -56,9 +60,13 @@ namespace AWS_FileProcessing
 
                 await fileTransferUtility.UploadAsync(fileTransferUtilityRequest);
                 Console.WriteLine("Upload 4 completed");
-            } catch (AmazonS3Exception e) {
+            }
+            catch (AmazonS3Exception e)
+            {
                 Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
             }
         }
