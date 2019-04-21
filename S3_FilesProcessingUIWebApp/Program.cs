7d90863762b675b6767b9665dcd092938313de73
Anna9701 <202340@edu.p.lodz.pl>
2019-04-21 15:20:58 +0200

    Added web app project

---
+++ S3_FilesProcessingUIWebApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace S3_FilesProcessingUIWebApp {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
