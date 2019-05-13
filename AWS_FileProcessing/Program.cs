a6d31c703f73cdbe6ec3e97cc4bb8f94c5d7655a
Anna9701 <202340@edu.p.lodz.pl>
2019-05-13 13:10:46 +0200

    removed redundant API

--- AWS_FileProcessing/Program.cs
+++ AWS_FileProcessing/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using Amazon;
-using Amazon.S3;
+using Amazon.Runtime;
+using Amazon.SQS;
 using AWS_S3_FilleProcessingLib;
 
 namespace AWS_FileProcessing {
     internal class Program {
-        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUCentral1;
-
         private const String BucketName = "filemanipulationsappstorage1";
         private const String KeyName = "SampleFile";
         private const String FilePath = @"D:\source\repos\polibuda\AWS_App1\README.md";
+        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUCentral1;
 
         private static void Main(String[] args) {
             Console.WriteLine("Hello World!");
-            using (S3BucketFilesManager bucketFilesManager = new S3BucketFilesManager(BucketRegion)) {
-                bucketFilesManager.UploadFileAsync(FilePath, BucketName).Wait();
-            }
+            AmazonSQSConfig amazonSQSConfig = new AmazonSQSConfig {ServiceURL = "https://sqs.eu-central-1.amazonaws.com/" };
+            AmazonSQSClient client = new AmazonSQSClient(BucketRegion);
+            var x = client.ListQueuesAsync("FilesProcessingQueue").Result;
         }
     }
 }
