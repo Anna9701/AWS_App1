5c2a6373c97e07077bca6ac535a8fff4acd36e20
Anna9701 <202340@edu.p.lodz.pl>
2019-06-01 07:28:37 +0200

    Added sqs library

--- AWS_FileProcessing/Program.cs
+++ AWS_FileProcessing/Program.cs
@@ -1,21 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.Runtime;
 using Amazon.SQS;
 using AWS_S3_FilleProcessingLib;
+using AWS_SQSLibrary;
 
 namespace AWS_FileProcessing {
     internal class Program {
         private const String BucketName = "filemanipulationsappstorage1";
         private const String KeyName = "SampleFile";
         private const String FilePath = @"D:\source\repos\polibuda\AWS_App1\README.md";
+        private const String QueueUrl = "https://sqs.eu-central-1.amazonaws.com/650110513798/FilesProcessingQueue";
         private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUCentral1;
-
+        
         private static void Main(String[] args) {
-            Console.WriteLine("Hello World!");
-            AmazonSQSConfig amazonSQSConfig = new AmazonSQSConfig {ServiceURL = "https://sqs.eu-central-1.amazonaws.com/" };
-            AmazonSQSClient client = new AmazonSQSClient(BucketRegion);
-            var x = client.ListQueuesAsync("FilesProcessingQueue").Result;
+            AwsSqsHandler sqsHandler = new AwsSqsHandler(QueueUrl, BucketRegion);
+            new TaskFactory().StartNew(async () => {
+                while (true) {
+                    var messages = await sqsHandler.ReceiveMessage();
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    await sqsHandler.DeleteMessage(messages[0]);
+                }
+            });
+            Console.ReadLine();
         }
+
+
     }
 }
---
+++ AWS_SQSLibrary/AwsSqsHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace AWS_SQSLibrary
+{
+    public class AwsSqsHandler {
+        private readonly String _queueUrl;
+        private readonly AmazonSQSClient _client;
+
+        public AwsSqsHandler(String queueUrl, RegionEndpoint bucketRegion) {
+            _queueUrl = queueUrl;
+            _client = new AmazonSQSClient(bucketRegion);
+        }
+
+        public async Task<List<Message>> ReceiveMessage() {
+            ReceiveMessageResponse message = await _client.ReceiveMessageAsync(_queueUrl);
+            return message.Messages;
+        }
+
+        public async Task<HttpStatusCode> SendMessage(String messageContent) {
+            SendMessageResponse message = await _client.SendMessageAsync(_queueUrl, messageContent);
+            return message.HttpStatusCode;
+        }
+
+        public async Task<HttpStatusCode> DeleteMessage(Message messageToRemove) {
+            DeleteMessageResponse deleteMessageResponse = await _client.DeleteMessageAsync(_queueUrl, messageToRemove.ReceiptHandle);
+            return deleteMessageResponse.HttpStatusCode;
+        }
+    }
+}
