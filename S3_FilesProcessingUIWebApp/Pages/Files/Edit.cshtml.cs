57e07237d65459a2e857c94f0b77fd6f298a1d79
Anna9701 <202340@edu.p.lodz.pl>
2019-06-01 11:09:27 +0200

    added page for editing files

---
+++ S3_FilesProcessingUIWebApp/Pages/Files/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3.Model;
+using AWS_S3_FilleProcessingLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace S3_FilesProcessingUIWebApp.Pages.Files {
+    public class EditModel : PageModel {
+        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUCentral1;
+        private readonly String _bucketName;
+
+        [BindProperty]
+        public String FileToMergeWith { get; set; }
+        
+        public List<S3Object> FilesInBucket { get; set; }
+
+        public EditModel(IConfiguration configuration) {
+            _bucketName = configuration["BucketName"];
+        }
+
+        [HttpGet]
+        public async Task OnGetAsync(String fileKey) {
+            using (S3BucketFilesManager bucketFilesManager = new S3BucketFilesManager(BucketRegion)) {
+                FilesInBucket = (await bucketFilesManager.ListFilesAsync(_bucketName)).Where(file => file.Key != fileKey).ToList();
+            }
+
+            TempData["fileKey"] = fileKey;
+        }
+
+        [HttpPost]
+        public async Task<RedirectToPageResult> OnPostAsync() {
+            var key = TempData["fileKey"];
+
+            return RedirectToPage("/Files/Edit", key);
+        }
+    }
+}
