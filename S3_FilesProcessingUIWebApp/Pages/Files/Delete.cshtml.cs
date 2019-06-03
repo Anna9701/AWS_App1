c47a9e482270baf6cda318aa183712842f85e923
Anna9701 <202340@edu.p.lodz.pl>
2019-06-03 15:31:21 +0200

    Removed details link

---
+++ S3_FilesProcessingUIWebApp/Pages/Files/Delete.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace S3_FilesProcessingUIWebApp.Pages.Files {
+    public class DeleteModel : PageModel {
+        public async Task OnGetAsync(String fileKey) {
+        }
+    }
+}
